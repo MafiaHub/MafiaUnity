250fc1ff69b40ebf58005db006bce0b70b3a5bf8
Dominik Madarász <zaklaus@outlook.com>
2018-07-01 15:11:44 +0200

    Refactor ModelGenerator

--- Assets/Scripts/Generators/ModelGenerator.cs
+++ Assets/Scripts/Generators/ModelGenerator.cs
@@ -34,7 +34,6 @@ namespace OpenMafia
             using (BinaryReader reader = new BinaryReader(fs))
             {
                 var modelLoader = new MafiaFormats.Reader4DS();
-                var bmp = new BMPLoader();
                 var model = modelLoader.loadModel(reader);
 
                 var meshId = 0;
@@ -48,14 +47,14 @@ namespace OpenMafia
                     var meshRenderer = child.GetComponent<MeshRenderer>();
 
                     children.Add(new KeyValuePair<int, Transform>(mafiaMesh.parentID, child.transform));
-                    
+
                     if (mafiaMesh.meshType != MafiaFormats.MeshType.MESHTYPE_STANDARD)
                         continue;
 
                     if (mafiaMesh.standard.instanced != 0)
                         continue;
-
-                    MafiaFormats.LOD firstMafiaLOD;
+                    
+                    Material[] materials;
 
                     switch (mafiaMesh.visualMeshType)
                     {
@@ -63,14 +62,19 @@ namespace OpenMafia
                         {
                             // TODO build up more lods
                             if (mafiaMesh.standard.lods.Count > 0)
-                                firstMafiaLOD = mafiaMesh.standard.lods[0];
+                            {
+                                meshFilter.mesh = GenerateMesh(mafiaMesh, mafiaMesh.standard.lods[0], model, out materials);
+                                meshRenderer.materials = materials;
+                            }
                             else
                                 continue;
-                        }break;
+                        }
+                        break;
 
                         case MafiaFormats.VisualMeshType.VISUALMESHTYPE_SINGLEMORPH:
                         {
-                            firstMafiaLOD = mafiaMesh.singleMorph.singleMesh.standard.lods[0];
+                            meshFilter.mesh = GenerateMesh(mafiaMesh, mafiaMesh.singleMorph.singleMesh.standard.lods[0], model, out materials);
+                            meshRenderer.materials = materials;
                         }
                         break;
 
@@ -79,128 +83,6 @@ namespace OpenMafia
                         default: continue;
                     }
                     
-                    List<Material> mats = new List<Material>();
-
-                    List<Vector3> unityVerts = new List<Vector3>();
-                    List<Vector3> unityNormals = new List<Vector3>();
-                    List<Vector2> unityUV = new List<Vector2>();
-
-                    foreach (var vert in firstMafiaLOD.vertices)
-                    {
-                        unityVerts.Add(vert.pos);
-                        unityNormals.Add(vert.normal);
-                        unityUV.Add(new Vector2(vert.uv.x, -1 * vert.uv.y));
-                    }
-
-                    var mesh = new Mesh();
-                    mesh.name = mafiaMesh.meshName;
-
-                    mesh.SetVertices(unityVerts);
-                    mesh.SetUVs(0, unityUV);
-                    mesh.SetNormals(unityNormals);
-                    meshFilter.mesh = mesh;
-
-                    mesh.subMeshCount = firstMafiaLOD.faceGroups.Count;
-
-                    var faceGroupId = 0;
-
-                    foreach (var faceGroup in firstMafiaLOD.faceGroups)
-                    {
-                        List<int> unityIndices = new List<int>();
-                        foreach (var face in faceGroup.faces)
-                        {
-                            unityIndices.Add(face.a);
-                            unityIndices.Add(face.b);
-                            unityIndices.Add(face.c);
-                        }
-
-                        mesh.SetTriangles(unityIndices.ToArray(), faceGroupId);
-
-                        var matId = (int)Mathf.Max(0, Mathf.Min(model.materials.Count - 1, faceGroup.materialID - 1));
-
-                        if (model.materials.Count > 0)
-                        {
-                            var mafiaMat = model.materials[matId];
-
-                            Material mat;
-
-                            if ((mafiaMat.flags & MafiaFormats.MaterialFlag.MATERIALFLAG_COLORKEY) != 0)
-                            {
-                                mat = new Material(Shader.Find("Standard"));
-                                mat.SetFloat("_Mode", 1f); // Set rendering mode to Cutout
-                                mat.SetFloat("_Glossiness", 0f);
-                                mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
-                                mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-                                mat.SetInt("_ZWrite", 0);
-                                mat.DisableKeyword("_ALPHATEST_ON");
-                                mat.EnableKeyword("_ALPHABLEND_ON");
-                                mat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-                                mat.renderQueue = 3000;
-                            }
-                            else if (mafiaMat.transparency < 1)
-                            {
-                                mat = new Material(Shader.Find("Standard"));
-                                mat.SetFloat("_Mode", 3f); // Set rendering mode to Transparent
-                                mat.SetFloat("_Glossiness", 0f);
-                                mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
-                                mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-                                mat.SetInt("_ZWrite", 0);
-                                mat.DisableKeyword("_ALPHATEST_ON");
-                                mat.EnableKeyword("_ALPHABLEND_ON");
-                                mat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-                                mat.renderQueue = 3000;
-                            }
-                            else
-                            {
-                                mat = new Material(Shader.Find("Standard"));
-                                mat.SetFloat("_Glossiness", 0f);
-                            }
-
-                            //if (matId > 0)
-                            {
-
-                                // TODO support more types as well as transparency
-
-                                if (mafiaMat.diffuseMapName != null ||
-                                    mafiaMat.alphaMapName != null)
-                                {
-                                    if ((mafiaMat.flags & MafiaFormats.MaterialFlag.MATERIALFLAG_COLORKEY) != 0)
-                                        BMPLoader.useTransparencyKey = true;
-
-                                    BMPImage image = null;
-
-                                    if ((mafiaMat.flags & MafiaFormats.MaterialFlag.MATERIALFLAG_TEXTUREDIFFUSE) != 0)
-                                        image = bmp.LoadBMP(GameManager.instance.fileSystem.GetCanonicalPath("maps/" + mafiaMat.diffuseMapName));
-                                    else if (mafiaMat.alphaMapName != null)
-                                        image = bmp.LoadBMP(GameManager.instance.fileSystem.GetCanonicalPath("maps/" + mafiaMat.alphaMapName));
-
-                                    BMPLoader.useTransparencyKey = false;
-
-                                    if (image != null)
-                                    {
-                                        Texture2D tex = image.ToTexture2D();
-
-                                        if ((mafiaMat.flags & MafiaFormats.MaterialFlag.MATERIALFLAG_TEXTUREDIFFUSE) != 0)
-                                            tex.name = mafiaMat.diffuseMapName;
-                                        else if (mafiaMat.alphaMapName != null)
-                                            tex.name = mafiaMat.alphaMapName;
-
-                                        mat.SetTexture("_MainTex", tex);
-                                    }
-
-                                    if (mafiaMat.transparency < 1)
-                                        mat.SetColor("_Color", new Color32(255, 255, 255, (byte)(mafiaMat.transparency * 255)));
-                                }
-                            }
-
-                            mats.Add(mat);
-                        }
-                        
-                        faceGroupId++;
-                    }
-
-                    meshRenderer.materials = mats.ToArray();
-
                     meshId++;
                 }
 
@@ -228,6 +110,134 @@ namespace OpenMafia
             fs.Close();
 
             return rootObject;
+        }
+
+        Mesh GenerateMesh(MafiaFormats.Mesh mafiaMesh, MafiaFormats.LOD firstMafiaLOD, MafiaFormats.Model model, out Material[] materials)
+        {
+            var mesh = new Mesh();
+            
+            var bmp = new BMPLoader();
+            List<Material> mats = new List<Material>();
+
+            List<Vector3> unityVerts = new List<Vector3>();
+            List<Vector3> unityNormals = new List<Vector3>();
+            List<Vector2> unityUV = new List<Vector2>();
+
+            foreach (var vert in firstMafiaLOD.vertices)
+            {
+                unityVerts.Add(vert.pos);
+                unityNormals.Add(vert.normal);
+                unityUV.Add(new Vector2(vert.uv.x, -1 * vert.uv.y));
+            }
+            
+            mesh.name = mafiaMesh.meshName;
+
+            mesh.SetVertices(unityVerts);
+            mesh.SetUVs(0, unityUV);
+            mesh.SetNormals(unityNormals);
+
+            mesh.subMeshCount = firstMafiaLOD.faceGroups.Count;
+
+            var faceGroupId = 0;
+
+            foreach (var faceGroup in firstMafiaLOD.faceGroups)
+            {
+                List<int> unityIndices = new List<int>();
+                foreach (var face in faceGroup.faces)
+                {
+                    unityIndices.Add(face.a);
+                    unityIndices.Add(face.b);
+                    unityIndices.Add(face.c);
+                }
+
+                mesh.SetTriangles(unityIndices.ToArray(), faceGroupId);
+
+                var matId = (int)Mathf.Max(0, Mathf.Min(model.materials.Count - 1, faceGroup.materialID - 1));
+
+                if (model.materials.Count > 0)
+                {
+                    var mafiaMat = model.materials[matId];
+
+                    Material mat;
+
+                    if ((mafiaMat.flags & MafiaFormats.MaterialFlag.MATERIALFLAG_COLORKEY) != 0)
+                    {
+                        mat = new Material(Shader.Find("Standard"));
+                        mat.SetFloat("_Mode", 1f); // Set rendering mode to Cutout
+                        mat.SetFloat("_Glossiness", 0f);
+                        mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+                        mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                        mat.SetInt("_ZWrite", 0);
+                        mat.DisableKeyword("_ALPHATEST_ON");
+                        mat.EnableKeyword("_ALPHABLEND_ON");
+                        mat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                        mat.renderQueue = 3000;
+                    }
+                    else if (mafiaMat.transparency < 1)
+                    {
+                        mat = new Material(Shader.Find("Standard"));
+                        mat.SetFloat("_Mode", 3f); // Set rendering mode to Transparent
+                        mat.SetFloat("_Glossiness", 0f);
+                        mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+                        mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                        mat.SetInt("_ZWrite", 0);
+                        mat.DisableKeyword("_ALPHATEST_ON");
+                        mat.EnableKeyword("_ALPHABLEND_ON");
+                        mat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                        mat.renderQueue = 3000;
+                    }
+                    else
+                    {
+                        mat = new Material(Shader.Find("Standard"));
+                        mat.SetFloat("_Glossiness", 0f);
+                    }
+
+                    //if (matId > 0)
+                    {
+
+                        // TODO support more types as well as transparency
+
+                        if (mafiaMat.diffuseMapName != null ||
+                            mafiaMat.alphaMapName != null)
+                        {
+                            if ((mafiaMat.flags & MafiaFormats.MaterialFlag.MATERIALFLAG_COLORKEY) != 0)
+                                BMPLoader.useTransparencyKey = true;
+
+                            BMPImage image = null;
+
+                            if ((mafiaMat.flags & MafiaFormats.MaterialFlag.MATERIALFLAG_TEXTUREDIFFUSE) != 0)
+                                image = bmp.LoadBMP(GameManager.instance.fileSystem.GetCanonicalPath("maps/" + mafiaMat.diffuseMapName));
+                            else if (mafiaMat.alphaMapName != null)
+                                image = bmp.LoadBMP(GameManager.instance.fileSystem.GetCanonicalPath("maps/" + mafiaMat.alphaMapName));
+
+                            BMPLoader.useTransparencyKey = false;
+
+                            if (image != null)
+                            {
+                                Texture2D tex = image.ToTexture2D();
+
+                                if ((mafiaMat.flags & MafiaFormats.MaterialFlag.MATERIALFLAG_TEXTUREDIFFUSE) != 0)
+                                    tex.name = mafiaMat.diffuseMapName;
+                                else if (mafiaMat.alphaMapName != null)
+                                    tex.name = mafiaMat.alphaMapName;
+
+                                mat.SetTexture("_MainTex", tex);
+                            }
+
+                            if (mafiaMat.transparency < 1)
+                                mat.SetColor("_Color", new Color32(255, 255, 255, (byte)(mafiaMat.transparency * 255)));
+                        }
+                    }
+
+                    mats.Add(mat);
+                }
+
+                faceGroupId++;
+            }
+
+            materials = mats.ToArray();
+
+            return mesh;
         }
     }
 }
