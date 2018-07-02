e50a9f4c7571f005010f7da7232c8c43d343ae14
Dominik Madarász <zaklaus@outlook.com>
2018-07-02 19:05:22 +0200

    Animated textures support

--- Assets/Scripts/Generators/ModelGenerator.cs
+++ Assets/Scripts/Generators/ModelGenerator.cs
@@ -63,7 +63,7 @@ namespace OpenMafia
                             // TODO build up more lods
                             if (mafiaMesh.standard.lods.Count > 0)
                             {
-                                meshFilter.mesh = GenerateMesh(mafiaMesh, mafiaMesh.standard.lods[0], model, out materials);
+                                meshFilter.mesh = GenerateMesh(mafiaMesh, child, mafiaMesh.standard.lods[0], model, out materials);
                                 meshRenderer.materials = materials;
                             }
                             else
@@ -73,7 +73,7 @@ namespace OpenMafia
 
                         case MafiaFormats.VisualMeshType.VISUALMESHTYPE_SINGLEMORPH:
                         {
-                            meshFilter.mesh = GenerateMesh(mafiaMesh, mafiaMesh.singleMorph.singleMesh.standard.lods[0], model, out materials);
+                            meshFilter.mesh = GenerateMesh(mafiaMesh, child, mafiaMesh.singleMorph.singleMesh.standard.lods[0], model, out materials);
                             meshRenderer.materials = materials;
                         }
                         break;
@@ -112,7 +112,7 @@ namespace OpenMafia
             return rootObject;
         }
 
-        Mesh GenerateMesh(MafiaFormats.Mesh mafiaMesh, MafiaFormats.LOD firstMafiaLOD, MafiaFormats.Model model, out Material[] materials)
+        Mesh GenerateMesh(MafiaFormats.Mesh mafiaMesh, GameObject ent, MafiaFormats.LOD firstMafiaLOD, MafiaFormats.Model model, out Material[] materials)
         {
             var mesh = new Mesh();
             
@@ -229,6 +229,49 @@ namespace OpenMafia
 
                             if (mafiaMat.transparency < 1)
                                 mat.SetColor("_Color", new Color32(255, 255, 255, (byte)(mafiaMat.transparency * 255)));
+
+                            if ((mafiaMat.flags & (MafiaFormats.MaterialFlag.MATERIALFLAG_ANIMATEDTEXTUREDIFFUSE | MafiaFormats.MaterialFlag.MATERIALFLAG_ANIMATEXTEXTUREALPHA)) != 0)
+                            {
+                                List<Texture2D> frames = new List<Texture2D>();
+
+                                string fileName = null;
+
+                                if ((mafiaMat.flags & MafiaFormats.MaterialFlag.MATERIALFLAG_ANIMATEDTEXTUREDIFFUSE) != 0)
+                                    fileName = mafiaMat.diffuseMapName;
+                                else
+                                    fileName = mafiaMat.alphaMapName;
+
+                                if ((mafiaMat.flags & MafiaFormats.MaterialFlag.MATERIALFLAG_COLORKEY) != 0)
+                                    BMPLoader.useTransparencyKey = true;
+
+                                if (fileName != null)
+                                {
+                                    var path = fileName.Split('.');
+                                    string baseName = path[0];
+                                    string ext = path[1];
+
+                                    baseName = baseName.Substring(0, baseName.Length - 2);
+
+                                    for (int k = 0; k < mafiaMat.animSequenceLength; k++)
+                                    {
+                                        var frameImage = bmp.LoadBMP(GameManager.instance.fileSystem.GetCanonicalPath("maps/" + baseName + "0" + k + "." + ext));
+
+                                        if (frameImage == null)
+                                            continue;
+
+                                        var frame = frameImage.ToTexture2D();
+                                        frames.Add(frame);
+                                    }
+
+                                    var framePlayer = ent.AddComponent<TextureAnimationPlayer>();
+
+                                    framePlayer.frames = frames;
+                                    framePlayer.framePeriod = mafiaMat.framePeriod;
+                                    framePlayer.material = mat;
+                                }
+
+                                BMPLoader.useTransparencyKey = false;
+                            }
                         }
                     }
 
---
+++ Assets/Scripts/Utils/TextureAnimationPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureAnimationPlayer : MonoBehaviour {
+
+    public List<Texture2D> frames;
+    public Material material;
+    public uint framePeriod;
+
+    private float timeUntilNextFrame;
+    private int frameIndex;
+
+    private void Start()
+    {
+        ResetTime();
+    }
+
+    void Update () {
+        if (timeUntilNextFrame < 0)
+        {
+            ResetTime();
+
+            material.SetTexture("_MainTex", frames[frameIndex++]);
+
+            if (frameIndex >= frames.Count)
+                frameIndex = 0;
+        }
+        else
+            timeUntilNextFrame -= Time.deltaTime;
+	}
+
+    void ResetTime()
+    {
+        timeUntilNextFrame = framePeriod / 1000f;
+    }
+}
