0c862fd3df4e24ba198fe8ec3fd72ca6b736201b
Dominik Madarász <zaklaus@outlook.com>
2018-07-13 13:42:34 +0200

    WIP anim blending

--- Assets/Scripts/Utils/ModelAnimationPlayer.cs
+++ Assets/Scripts/Utils/ModelAnimationPlayer.cs
@@ -17,19 +17,27 @@ namespace MafiaUnity
     {
         public bool isPlaying = false;
         public AnimationPlaybackMode playbackMode;
-        
+        public float blendBeginPercentage;
+        public float blendEndPercentage;
+        [SerializeField] public MafiaAnimation mafiaAnimation;
+        [SerializeField] public MafiaAnimation pairAnimation;
+
         private int[] posFrameId;
         private int[] rotFrameId;
         private int[] scaleFrameId;
         private const float frameStep = 1f / 25f;
         private float frameTime;
-        [SerializeField] public MafiaAnimation mafiaAnimation = new MafiaAnimation();
+        
+        public MafiaAnimation LoadAndSetAnimation(string animName)
+        {
+            mafiaAnimation = LoadAnimation(animName);
+            return mafiaAnimation;
+        }
 
         public MafiaAnimation LoadAnimation(string animName)
         {
-            AnimReset();
-            mafiaAnimation = new MafiaAnimation();
-            mafiaAnimation.animationSequences = new List<AnimationSequence>();
+            var animation = new MafiaAnimation();
+            animation.animationSequences = new List<AnimationSequence>();
 
             FileStream fs;
 
@@ -51,11 +59,11 @@ namespace MafiaUnity
                 {
                     var newAnimationSequence = new AnimationSequence();
                     newAnimationSequence.loaderSequence = seq;
-                    mafiaAnimation.animationSequences.Add(newAnimationSequence);
+                    animation.animationSequences.Add(newAnimationSequence);
                 }
             }
 
-            return mafiaAnimation;
+            return animation;
         }
 
         public void SetAnimation(MafiaAnimation anim)
@@ -86,9 +94,20 @@ namespace MafiaUnity
                 animationSeq.Reset();
         }
 
+        public void BlendAnimation(MafiaAnimation anim)
+        {
+            if (anim == null || mafiaAnimation == null)
+                return;
+
+            pairAnimation = anim;
+        }
+
         void Update()
         {
             if (!isPlaying)
+                return;
+
+            if (mafiaAnimation == null || mafiaAnimation.animationSequences == null)
                 return;
 
             if (IsFinished())
@@ -175,6 +194,51 @@ namespace MafiaUnity
             return movementResult && rotationResult && scaleResult;
         }
 
+        public class AnimationTransform
+        {
+            public Vector3 currentPosition;
+            public Vector3 nextPosition;
+
+            public Quaternion currentRotation;
+            public Quaternion nextRotation;
+
+            public Vector3 currentScale;
+            public Vector3 nextScale;
+        }
+
+        public AnimationTransform GetCurrentMotion()
+        {
+            var motion = new AnimationTransform();
+
+
+            if (loaderSequence.hasMovement())
+            {
+                motion.currentPosition = loaderSequence.positions[positionKeyFrameId];
+                motion.nextPosition = loaderSequence.positions[positionKeyFrameId + 1];
+            }
+
+            if (loaderSequence.hasRotation())
+            {
+                motion.currentRotation = new Quaternion(loaderSequence.rotations[rotationKeyFrameId].y,
+                    loaderSequence.rotations[rotationKeyFrameId].z,
+                    loaderSequence.rotations[rotationKeyFrameId].w,
+                    -1 * loaderSequence.rotations[rotationKeyFrameId].x);
+
+                motion.nextRotation = new Quaternion(loaderSequence.rotations[rotationKeyFrameId + 1].y,
+                    loaderSequence.rotations[rotationKeyFrameId + 1].z,
+                    loaderSequence.rotations[rotationKeyFrameId + 1].w,
+                    -1 * loaderSequence.rotations[rotationKeyFrameId + 1].x);
+            }
+
+            if (loaderSequence.hasScale())
+            {
+                motion.currentScale = loaderSequence.scales[scaleKeyFrameId];
+                motion.nextScale = loaderSequence.scales[scaleKeyFrameId + 1];
+            }
+
+            return motion;
+        }
+
         public void Update(float deltaLerp, Transform rootObject)
         {
             if (loaderSequence == null)
@@ -185,31 +249,21 @@ namespace MafiaUnity
             if (boneTransform == null)
                 return;
 
+            var tr = GetCurrentMotion();
+
             if (loaderSequence.hasMovement())
             {
-                boneTransform.localPosition = Vector3.Lerp(loaderSequence.positions[positionKeyFrameId],
-                    loaderSequence.positions[positionKeyFrameId + 1], deltaLerp);
+                boneTransform.localPosition = Vector3.Lerp(tr.currentPosition, tr.nextPosition, deltaLerp);
             }
 
             if (loaderSequence.hasRotation())
             {
-                var tmpRot = new Quaternion(loaderSequence.rotations[rotationKeyFrameId].y,
-                    loaderSequence.rotations[rotationKeyFrameId].z,
-                    loaderSequence.rotations[rotationKeyFrameId].w,
-                    -1 * loaderSequence.rotations[rotationKeyFrameId].x);
-
-                var tmpRot2 = new Quaternion(loaderSequence.rotations[rotationKeyFrameId + 1].y,
-                    loaderSequence.rotations[rotationKeyFrameId + 1].z,
-                    loaderSequence.rotations[rotationKeyFrameId + 1].w,
-                    -1 * loaderSequence.rotations[rotationKeyFrameId + 1].x);
-
-                boneTransform.localRotation = Quaternion.Slerp(tmpRot, tmpRot2, deltaLerp);
+                boneTransform.localRotation = Quaternion.Slerp(tr.currentRotation, tr.nextRotation, deltaLerp);
             }
 
             if (loaderSequence.hasScale())
             {
-                boneTransform.localScale = Vector3.Lerp(loaderSequence.scales[scaleKeyFrameId],
-                    loaderSequence.scales[scaleKeyFrameId + 1], deltaLerp);
+                boneTransform.localScale = Vector3.Lerp(tr.currentScale, tr.nextScale, deltaLerp);
             }
         }
     }
@@ -232,7 +286,8 @@ namespace MafiaUnity
                 {
                     var animPlayer = target as ModelAnimationPlayer;
 
-                    animPlayer.LoadAnimation("anims/" + animName + ".5ds");
+                    var anim = animPlayer.LoadAndSetAnimation("anims/" + animName + ".5ds");
+                    animPlayer.AnimReset();
                     animPlayer.isPlaying = true;
                 }
             }
