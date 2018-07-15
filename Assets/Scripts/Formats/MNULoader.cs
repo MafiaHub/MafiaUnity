c9af407f9ace5c56672ca17074eedcef48fa3964
DavoSK <david.svitana@gmail.com>
2018-07-15 19:04:01 +0200

    Added missing 4byte padding in header of mnu parser

--- Assets/Scripts/Formats/MNULoader.cs
+++ Assets/Scripts/Formats/MNULoader.cs
@@ -12,21 +12,22 @@ namespace MafiaUnity
     {
         public unsafe struct Header
         {
-            public fixed char magic[4];
+            public fixed byte magic[4];
             public uint unknown;
             public uint numControls;
+            public uint padding;
         }
 
         public unsafe struct Control
         {
-            public uint unknown;
-            public fixed char type[4];
-            Vector2 pos;
-            float scaleX;
-            float scaleY;
-            uint textId;
-            ushort textColor;
-            ushort bgColor;
+            public uint id;
+            public fixed byte type[4];
+            public Vector2 pos;
+            public float scaleX;
+            public float scaleY;
+            public uint textId;
+            public ushort textColor;
+            public ushort bgColor;
         }
 
         public List<Control> controls = new List<Control>();
@@ -44,18 +45,18 @@ namespace MafiaUnity
             Header newHeader = new Header();
             ReadStruct((byte*)&newHeader, reader.ReadBytes(sizeof(Header)));
 
-            var menuString = new string(newHeader.magic);
-
-            if (menuString != "Menu") 
+            if (newHeader.magic[0] != 'M' || newHeader.magic[3] != 'u') 
             {
                 Debug.Log("Wrong mnu file !");
                 return;
             }
 
+            
             for (var i = 0; i < newHeader.numControls; i++)
             {
                 Control newControl = new Control();
                 ReadStruct((byte*)&newControl, reader.ReadBytes(sizeof(Control)));
+
                 controls.Add(newControl);
             }
         }
