c1dfc41edd6c9e64a07387defdb79c929ef68077
Dominik Madarász <zaklaus@outlook.com>
2018-08-09 06:26:42 +0200

    Indent using spaces zak, please

--- Assets/Scripts/Utils/MissionHacks.cs
+++ Assets/Scripts/Utils/MissionHacks.cs
@@ -4,31 +4,31 @@ using UnityEngine;
 
 namespace MafiaUnity
 {
-	/* 
-		This file consists of hacks used to modify game's scene depending on the mission loaded.
-	*/
-	public class MissionHacks
-	{
-		public MissionHacks(string missionName)
-		{
-			switch (missionName)
-			{
-				// example usage
-				case "tutorial":
-				{
-					var skybox = GameObject.Find("o_m_");
+    /* 
+        This file consists of hacks used to modify game's scene depending on the mission loaded.
+    */
+    public class MissionHacks
+    {
+        public MissionHacks(string missionName)
+        {
+            switch (missionName)
+            {
+                // example usage
+                case "tutorial":
+                {
+                    var skybox = GameObject.Find("o_m_");
 
-					if (skybox != null)
-					{
-	#if UNITY_EDITOR
-							GameObject.DestroyImmediate(skybox);
-	#else
-							GameObject.Destroy(skybox);
-	#endif
-					}
-				}
-				break;
-			}
-		}
-	}
+                    if (skybox != null)
+                    {
+    #if UNITY_EDITOR
+                            GameObject.DestroyImmediate(skybox);
+    #else
+                            GameObject.Destroy(skybox);
+    #endif
+                    }
+                }
+                break;
+            }
+        }
+    }
 }
