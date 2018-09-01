4dee7b3183b8d62710075227a2a1a190f6c07842
Dominik Madarász <zaklaus@outlook.com>
2018-09-02 01:05:01 +0200

    Implement OGGLoader (Yay!)

---
+++ Assets/Scripts/Loaders/Formats/OGGLoader.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace MafiaUnity
+{
+    namespace MafiaFormats
+    {
+        public class OGGLoader
+        {
+            public AudioClip ToAudioClip(string name)
+            {
+                var vorbis = new NVorbis.VorbisReader(GameAPI.instance.fileSystem.GetStreamFromPath(Path.Combine("sounds", name + ".ogg")), true);
+
+                var channels = vorbis.Channels;
+                var sampleRate = vorbis.SampleRate;
+                var totalSamples = vorbis.TotalSamples;
+
+                AudioClip clip = AudioClip.Create(name, (int)totalSamples, channels, sampleRate, false);
+
+                var readBuffer = new float[channels * sampleRate / 5];
+                var totalBuffer = new float[channels * totalSamples];
+
+                int rd = 0;
+                int prev = 0;
+                while ((rd = vorbis.ReadSamples(readBuffer, 0, readBuffer.Length)) > 0)
+                {
+                    Buffer.BlockCopy(readBuffer, 0, totalBuffer, prev * sizeof(float), rd * sizeof(float));
+
+                    prev += rd;
+                }
+
+                clip.SetData(totalBuffer, 0);
+
+                return clip;
+            }
+        }
+    }
+}
